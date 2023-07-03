bdb276a26b629e80c0b48f13854e768066ba7049
Hausel Kinga <hausel.kinga@gmail.com>
2023-07-03 12:53:28 +0200

    implement save on bill type

--- bill.go
+++ bill.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 type bill struct {
 	name  string
@@ -47,4 +50,15 @@ func (b *bill) updateTip(tip float64) {
 // add item to the bill
 func (b *bill) addItem(name string, price float64) {
 	b.items[name] = price
+}
+
+// save bill
+func (b *bill) save() {
+	data := []byte(b.format())
+
+	err := os.WriteFile("bills/"+b.name+".txt", data, 0644)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("bill was saved to file")
 }
--- main.go
+++ main.go
@@ -58,7 +58,8 @@ func promptOptions(b bill) {
 		fmt.Println("Tip added -", tip)
 		promptOptions(b)
 	case "s":
-		fmt.Println("you chose to save the bill", b)
+		b.save()
+		fmt.Println("you saved the file -", b.name)
 	default:
 		fmt.Println("that was not a valid option...")
 		promptOptions(b)
