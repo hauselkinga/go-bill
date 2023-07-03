2aeef52baed6012f7343e176dd61bef95521c340
Hausel Kinga <hausel.kinga@gmail.com>
2023-07-03 11:36:21 +0200

    add createBill() and getInput() functions

--- main.go
+++ main.go
@@ -1,16 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func getInput(prompt string, r *bufio.Reader) (string, error) {
+	fmt.Print(prompt)
+	input, err := r.ReadString('\n')
+
+	return strings.TrimSpace(input), err
+}
+
+func createBill() bill {
+	reader := bufio.NewReader(os.Stdin)
+
+	name, _ := getInput("Create a new bill name: ", reader)
+
+	b := newBill(name)
+	fmt.Println("Created the bill -", b.name)
+
+	return b
+}
 
 func main() {
-	mybill := newBill("mario's bill")
-
-	mybill.addItem("onion soup", 4.50)
-	mybill.addItem("veg pie", 8.95)
-	mybill.addItem("toffee pudding", 4.95)
-	mybill.addItem("coffee", 3.25)
-
-	mybill.updateTip(10)
+	mybill := createBill()
 
 	fmt.Println(mybill.format())
 }
